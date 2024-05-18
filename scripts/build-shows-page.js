9d21d67b9c1207e0a7566869ba278a8bb6805e38
Ruslan <ruslansemko.ac@gmail.com>
2024-05-19 00:06:24 +0100

    array of object created

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,8 @@
+const showList = [
+    { date: "Mon Sept 09 2024", venue: "Ronald Lane", location: "San Fransico, CA", button: "BUY TICKETS"},
+    { date: "Tue Sept 17 2024", venue: "Pier 3 East", location: "San Fransico, CA", button: "BUY TICKETS"},
+    { date: "Sat Oct 12 2024", venue: "View Lounge", location: "San Fransico, CA", button: "BUY TICKETS"},
+    { date: "Sat Nov 16 2024", venue: "Hyatt Agency", location: "San Fransico, CA", button: "BUY TICKETS"},
+    { date: "Fri Nov 29 2024", venue: "Moscow Center", location: "San Fransico, CA", button: "BUY TICKETS"},
+    { date: "Wed Dec 18 2024", venue: "Press Club", location: "San Fransico, CA", button: "BUY TICKETS"},
+]
